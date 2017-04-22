9cb377736abc126fc14a309c9bf8594c8871e147
Rafał Bielec <accounts@rbdevelopment.pl>
2017-04-22 18:27:22 +0200

    Refactored CurrencyReadOnlyBox into a function.

--- src/components/CurrencyROBox.jsx
+++ src/components/CurrencyROBox.jsx
@@ -3,21 +3,17 @@ import PropTypes from 'prop-types';
 import ControlLabel from './ControlLabel';
 import ContainerBox from './ContainerBox';
 
-class CurrencyReadOnlyBox extends React.Component {
-    render() {
-        return (
-            <ContainerBox>
-                <ControlLabel target={this.props.id} desc={this.props.desc} />
-                <div className="input-group">
-                    <span className="input-group-addon">
-                        <span className="glyphicon glyphicon-gbp" aria-hidden="true"></span>
-                    </span>
-                    <input value={this.props.amount.toFixed(2)} type="text" className="form-control" id={this.props.id} readOnly={true} />
-                </div>
-            </ContainerBox>
-        );
-    }
-}
+const CurrencyReadOnlyBox = ({ id, desc, amount }) => (
+    <ContainerBox>
+        <ControlLabel target={id} desc={desc} />
+        <div className="input-group">
+            <span className="input-group-addon">
+                <span className="glyphicon glyphicon-gbp" aria-hidden="true"></span>
+            </span>
+            <input value={amount.toFixed(2)} type="text" className="form-control" id={id} readOnly={true} />
+        </div>
+    </ContainerBox>
+);
 
 CurrencyReadOnlyBox.propTypes = {
     amount: PropTypes.number.isRequired,
